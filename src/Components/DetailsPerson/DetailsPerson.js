839c4ed3e31d396cc1aba329082fb675d208d8bc
Shariful Islam <sharifulmamun7@gmail.com>
2021-09-26 19:21:49 +0600

    initial commit

---
+++ src/Components/DetailsPerson/DetailsPerson.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const DetailsPerson = (props) => {
+    const {name,img,role} =props.person
+    return (
+        <div class="card my-3">
+  <div class="row g-0">
+    <div class="col-md-3 text-center p-2 justify-content-center align-items-center">
+      <img src={img} class="img-fluid rounded-circle " alt="..." />
+    </div>
+    <div class="col-md-9">
+      <div class="card-body">
+        <p class="card-title"><small><b>Name : {name}</b></small></p>
+        <p class="card-text"><small><b>Role: {role}</b></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default DetailsPerson;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,4 +1,3 @@
-
 import './Persons.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
@@ -9,25 +8,29 @@ const Persons = (props) => {
     const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
        
-  <div class="col">
-    <div class="card h-100">
-     <div class="text-center p-5">
-     <img src={img} class="card-img-top rounded-circle" height="200px" width="200px" alt="..."/>
+  <div className="col ">
+    <div class="card h-100  rounded-3 shadow">
+     <div class="text-center img-container p-2">
+     <img src={img} class="card-img-top rounded-circle "  alt="..."/>
      </div>
       <div class="card-body text-center">
-        <h5 class="card-title"><small><b>Name:</b>{name}</small></h5>
-        <p class="card-text"><b>Role:</b>{role}</p>
-        <p class="card-text"><b>Age:</b>{age}</p>
-        <p class="card-text"><b>Country:</b> {country}</p>
-        <p class="card-text"><b>Salary:</b> {salary}</p>
-        <p class="card-text"><b>Phone:</b> {phone}</p>
+        <h5 class="card-title"><small><b>Name: {name}</b></small></h5>
+        <p class="card-text"><b>Role: {role}</b></p>
+        <p class="card-text"><b>Age:{age} </b></p>
+        <p class="card-text"><b>Country:  {country}</b></p>
+        <p class="card-text"><b>Salary: ${salary} </b> </p>
+        <p class="card-text"><b>Phone: {phone} </b> </p>
+      <div className="d-flex justify-content-evenly">
+      <h2><a href=""><i class="fab fa-facebook"></i></a></h2>
+       <h2><a href=""><i class="fab fa-twitter"></i></a></h2>
       </div>
-      <div class="card-footer bg-white border-0">
+      </div>
+      <div class="card-footer card-container border-0 text-center">
         <button 
         onClick={ ()=> props.handleAddtoCart(props.person)}
         className=
-        "btn btn-success"
-        >{element} Add to cart</button>
+        "btn btn-custom w-100"
+        >{element} Add Person</button>
       </div>
     </div>
   </div>
